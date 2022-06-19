475a456654dc436760c8e8538d0c0ac637be529a
tmdgusya <tmdgusya@naver.com>
2022-06-19 14:49:08 +0900

    add Junit Dependency

--- build.gradle.kts
+++ build.gradle.kts
@@ -9,7 +9,13 @@ repositories {
     mavenCentral()
 }
 
+tasks.test {
+    useJUnitPlatform()
+}
+
 dependencies {
     implementation(kotlin("stdlib"))
     implementation("org.jetbrains.kotlinx:kotlinx-cli-jvm:0.3.4")
+    testImplementation(platform("org.junit:junit-bom:5.8.2"))
+    testImplementation("org.junit.jupiter:junit-jupiter")
 }
---
+++ src/test/kotlin/JunitPlayTest.kt
@@ -0,0 +1,10 @@
+import org.junit.jupiter.api.Assertions
+import org.junit.jupiter.api.Test
+
+class JunitPlayTest {
+
+    @Test
+    fun isPlay() {
+        Assertions.assertEquals(1, 1)
+    }
+}
